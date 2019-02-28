32394e2113afba5bace3a7a894a2d0b15fa5cf4f
朱彦霖 <zhuyanlin@renrenche.com>
2019-02-28 17:03:45 +0800

    多页面打包

---
+++ src/other.js
@@ -0,0 +1,1 @@
+console.log('morepage-other')
--- webpack.config.js
+++ webpack.config.js
@@ -1,111 +1,14 @@
-// webpack是node写出来的 node的写法
 let path = require('path')
-let HtmlWebpackPlugin = require('html-webpack-plugin')
-let MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-// let webpack = require('webpack')
 
 module.exports = {
-  optimization: { // 优化项
-    minimizer: [
-      new UglifyJsPlugin({ // 压缩js
-        cache: true,
-        parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      }),
-      new OptimizeCSSAssetsPlugin({}) // 压缩css
-    ]
+  mode: 'development',
+  // 多入口
+  entry: {
+    home: './src/index.js',
+    other: './src/other.js'
   },
-  devServer: { // 开发服务器的配置（webpack-dev-server）
-    port: 3000,
-    progress: true
-  },
-  mode: 'production', // 模式默认有两种production development开发模式代码不压缩看的清晰
-  entry: './src/index.js', // 入口文件
   output: {
-    filename: '/js/bundle.[chunkhash].js', // 打包后文件名
-    path: path.resolve(__dirname, 'dist'), //打包后路径必须是绝对路径resolve方法把相对路径解析成绝对路径，__dirname加不加都可以，它代表在当前目录下产生一个dist目录
-    publicPath: 'http://www.weilongyun.com' // 给所有打包文件引入时加前缀，包括css，js，img，如果只想处理图片可以单独在url-loader配置中加publicPath（上传七牛云等cdn加速时可用）
-  },
-  plugins: [ // 数组，放着所有webpack插件
-    new HtmlWebpackPlugin({ // 用于使用模板打包时生成index.html文件，并且在run dev时会将模板文件也打包到内存中
-      template: './index.html', // 模板文件
-      filename: 'index.html', // 打包后生成文件
-      hash: true, // 添加hash值解决缓存问题
-      minify: { // 对打包的html模板进行压缩
-        removeAttributeQuotes: true, // 删除属性双引号
-        collapseWhitespace: true // 折叠空行变成一行
-      }
-    }),
-    new MiniCssExtractPlugin({ // 抽离css样式
-      filename: '/css/main.[chunkhash].css'// 抽离出来的文件名
-    })
-    // new webpack.ProvidePlugin({ // 在每个模块中都注入$
-    //   $: 'jquery'
-    // })
-  ],
-  externals: {
-    jquery: "$"
-  },
-  module: { // 模块
-    // loader
-    rules: [ // 规则 loader的特点-希望单一功能
-      // css-loader-解析css文件包括css文件的引入（@import）语法等
-      // style-loader-把css文件插入到head标签中
-      {
-        test: /\.css$/,
-        // 字符串只用一个loader，多个loader用数组配置
-        // loader的顺序默认从右向左执行，从下到上执行
-        // 如果需要传参loader还可以配置成对象
-        use: [
-          // 'style-loader',
-          MiniCssExtractPlugin.loader, // 将css文件抽离出来，不再以style标签存放，而是创建link标签引入
-          'css-loader',
-          'postcss-loader'
-        ]
-      },
-      // 可处理less文件除此之外还有sass、stylus
-      // node-sass、sass-loader、stylus-loader
-      {
-        test: /\.less$/,
-        use: [
-          // 'style-loader', // 插入style标签
-          MiniCssExtractPlugin.loader, // 将less文件抽离出来，不再以style标签存放，而是创建link标签引入
-          'css-loader', // 解析路径
-          'postcss-loader',
-          'less-loader' // 把less转换成css
-        ]
-      },
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: { // 用babel-loader吧es6转换成es5
-              presets: [ // 预设规则
-                '@babel/preset-env'
-              ],
-              plugins: [ // 此处配置有顺序
-                ["@babel/plugin-proposal-decorators", { "legacy": true }],
-                ["@babel/plugin-proposal-class-properties", { "loose" : true }]
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: {
-          loader: 'url-loader',
-          // 做一个限制，当图片小于多少k时用base64转化，否则用file-loader将图片产出
-          options: {
-            limit: 2*1024,
-            // dist打包文件中的输出路径
-            outputPath: '/img/'
-          }
-        }
-      }
-    ]
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
   }
 }
