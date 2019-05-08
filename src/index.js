0b81fe85601aaaee5707d5e0d907b8cea7bbe2bd
zhuyanlin <136136557912163.com>
2019-05-08 11:44:25 +0800

    增加css样式都打包到js文件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import './style/index.css';
 
 render(
     <div>Hello React!</div>,
--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ const happyThreadPool = HappyPack.ThreadPool({ size: 3 });
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
     output: {
-        path: path.resolve(__dirname, './dist'), // 输出的路径
-        filename: 'bundle.js'  // 打包后文件
+        path: path.resolve(__dirname, './dist/js'), // 输出的路径
+        filename: '[name].[chunkhash].js'  // 打包后文件
     },
     module: {
         rules: [
@@ -31,17 +31,17 @@ module.exports = {
                 // 排除 node_modules 目录下的文件，node_modules 目录下的文件都是采用的 ES5 语法，没必要再通过 Babel 去转换
                 exclude: path.resolve(__dirname, 'node_modules'),
             },
-            // {
-            //     // 把对 .css 文件的处理转交给 id 为 css 的 HappyPack 实例
-            //     test: /\.css$/,
-            //     use: ['happypack/loader?id=css']
-            // }
+            {
+                // 把对 .css 文件的处理转交给 id 为 css 的 HappyPack 实例
+                test: /\.css$/,
+                use: ['happypack/loader?id=css']
+            }
         ]
     },
     plugins: [
       new HtmlWebPackPlugin({
         template: "./index.html",
-        filename: "./index.html"
+        filename: "../index.html"
       }),
       // 告诉 Webpack 使用了哪些动态链接库
       new DllReferencePlugin({
@@ -60,16 +60,16 @@ module.exports = {
         // 使用共享进程池中的子进程去处理任务
         threadPool: happyThreadPool,
       }),
-    //   new HappyPack({
-    //     id: 'css',
-    //     // 如何处理 .css 文件，用法和 Loader 配置中一样
-    //     loaders: ['css-loader'],
-    //     // 使用共享进程池中的子进程去处理任务
-    //     threadPool: happyThreadPool,
-    //   }),
-    //   new ExtractTextPlugin({
-    //     filename: `[name].css`,
-    //   }),
+      new HappyPack({
+        id: 'css',
+        // 如何处理 .css 文件，用法和 Loader 配置中一样
+        loaders: ['style-loader', 'css-loader'],
+        // 使用共享进程池中的子进程去处理任务
+        threadPool: happyThreadPool,
+      }),
+      // new ExtractTextPlugin({
+      //   filename: `[name].css`,
+      // }),
     ],
     devServer: {
       contentBase: path.join(__dirname, "dist"),
