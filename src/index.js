b05f92c000ac37c73accd8dbf620e4f82bc2ad72
zhuyanlin <136136557912163.com>
2019-05-09 14:55:44 +0800

    按需加载

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import str from './util.js';
 
 console.log(str);
 
+window.document.getElementById('btn').addEventListener('click', function () {
+    // 当按钮被点击后才去加载 show.js 文件，文件加载成功后执行文件导出的函数
+    import(/* webpackChunkName: "show" */ './show.js').then((show) => {
+        show.default('Webpack');
+    })
+});
+
 render(
     <div>Hello React!!!</div>,
     document.getElementById('app')
